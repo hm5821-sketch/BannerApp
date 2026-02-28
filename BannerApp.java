75315bfaeba697edcbb37e85142526df6e448714
hm5821-sketch <hm5821@srmist.edu.in>
2026-02-28 20:51:10 +0530

    added UC5

--- BannerApp.java
+++ BannerApp.java
@@ -1,13 +1,13 @@
 public class BannerApp{
 public static void main(String[] args) {
- String[] lines = new String[7];
- lines[0] = (String.join( " ","     ***     ", " *******    " , " *******   " , "   *****   "  ));
- lines[1] = (String.join( " "," **     **   ", " *      *   " , " *      *  " , " *         "  ));
- lines[2] = (String.join( " ","**        ** ", " *      *   " , " *      *  " , " *         "  ));
- lines[3] = (String.join( " ","**        ** ", " *******    " , " *******   " , "   *****   "  ));
- lines[4] = (String.join( " ","**        ** ", " *          " , " *         " , "         * "  ));
- lines[5] = (String.join( " "," **      **  ", " *          " , " *         " , "         * "  ));
- lines[6] = (String.join( " ","     ***     ", " *          " , " *         " , "  ******   "  ));
+ String[] lines = {
+ String.join( " ","     ***     ", " *******    " , " *******   " , "   *****   "  ),
+  String.join( " "," **     **   ", " *      *   " , " *      *  " , " *         "  ),
+  String.join( " ","**        ** ", " *      *   " , " *      *  " , " *         "  ),
+ String.join( " ","**        ** ", " *******    " , " *******   " , "   *****   "  ),
+ String.join( " ","**        ** ", " *          " , " *         " , "         * "  ),
+  String.join( " "," **      **  ", " *          " , " *         " , "         * "  ),
+ String.join( " ","     ***     ", " *          " , " *         " , "  ******   "  ) };
  for (int i = 0; i < lines.length ; i++) {
 	 System.out.println (lines[i]);
  }
