860e7580edecb25dcb6a9ccfc22bfc706de021bb
hm5821-sketch <hm5821@srmist.edu.in>
2026-03-03 12:14:19 +0530

    added UC8

--- BannerApp.java
+++ BannerApp.java
@@ -1,4 +1,4 @@
-public class BannerApp{
+/*public class BannerApp{
  static class CharacterPatternMap {
 	 private Character character;
 	 private String[] pattern;
@@ -44,6 +44,35 @@ public class BannerApp{
 		
 		String message = "OOPS";
 		printMessage(message,charMaps);
+	}
+}*/
+import java.util.HashMap;
+public class BannerApp {
+	public static HashMap<Character,String[]>createCharacterMap() {
+		HashMap<Character,String[]>charMap = new HashMap<>();
+		charMap.put('O',new String[] {"    ***   " ,"  *     * " ,"  *     * " ,"  *     * " ,"  *     * " ,"  *     * " ,"    ***   " });
+		charMap.put('P',new String[] {"  ****** " ,"  *    * " ,"  *    * " ,"  ****** " ,"  *      " ,"  *      " ,"  *      " });
+		charMap.put('S',new String[] {"  ***** " ,"  *     " ,"  *     " ,"  ***** " ,"      * " ,"      * " ,"  ***** " });
+		return charMap;
+	}
+	public static void displayBanner(String message,HashMap<Character,String[]>charMap) {
+		if (message == null || message.isEmpty()) return;
+		int patternHeight = charMap.get('O').length;
+		for(int line = 0; line < patternHeight;line++) {
+			StringBuilder sb = new StringBuilder();
+			for (char ch : message.toUpperCase().toCharArray()) {
+				if (charMap.containsKey(ch)) {
+					String[] pattern = charMap.get(ch);
+					sb.append(pattern[line]).append(" ");
+				}
+			}
+			System.out.println(sb.toString());
+		}
+	}
+	public static void main (String[] args) {
+		HashMap<Character,String[]>charMap=createCharacterMap();
+		String message = "OOPS";
+		displayBanner(message,charMap);
 	}
 }
  
